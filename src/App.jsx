a5b030046d751a28364b9e01819e7ce4705f25e9
1m4mr4f1 <imamrafi192005@gmail.com>
2025-12-14 23:17:13 +0700

    React Icons (Library and Custom)

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,20 @@ import React from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import SignInPage from "./pages/signin";
 import SignUpPage from "./pages/signup";
-import ErrorPage from "./pages/404"; // Pastikan path sesuai (misal: error.jsx atau 404.jsx)
-import DashboardPage from "./pages/dashboard"; // Import halaman dashboard
+import ErrorPage from "./pages/404"; 
+import DashboardPage from "./pages/dashboard"; 
+import BalancePage from "./pages/BalancePage"; 
 
 const App = () => {
   const myRouter = createBrowserRouter([
     {
       path: "/",
-      element: <DashboardPage />, // Menggunakan DashboardPage sebagai halaman root
+      element: <DashboardPage />,
       errorElement: <ErrorPage />,
+    },
+    {
+      path: "/balance", 
+      element: <BalancePage />,
     },
     {
       path: "/login",
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,7 @@ export default {
   theme: {
     extend: {
       colors: {
+
         "Special-Red2": "#b83016",
 
         "primary": "#299D91",
